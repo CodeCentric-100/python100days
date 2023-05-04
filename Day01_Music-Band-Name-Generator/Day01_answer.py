419c447632d2fc875d47da40e74f6def884eea7b
saket61195 <saket61195@gmail.com>
2023-05-05 00:00:15 +0530

    day 1

---
+++ Day01_Music-Band-Name-Generator/Day01_answer.py
@@ -0,0 +1,21 @@
+# Band Name Generator
+
+# Source Code
+
+# 1. Creates a greeting that welcomes the user to the Band Name Generator.
+print("Welcome to the Band Name Generator.")
+
+# 2. Ask the user for the name of the city that they grew up in.
+city = input("What's the name of the city you grew up in?\n")
+
+# 3. Ask the user for the name of their pet.
+pet_name = input("What's the name of your pet?\n")
+
+# 4. Combines the name of their city and pet and show them their band name.
+print("Band name is " + pet_name + " " + city)
+
+# This can also be written like this because of using f-string
+print(f"Your generated Music band-Name is {city} {pet_name} ")
+
+# 5. Make sure the input cursor shows on a new line
+# \n means new line, the input given is printed on a new line
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # code python in 100 days
+100 Days of Code: The Complete Python Pro Bootcamp By Dr. Angela Yu
